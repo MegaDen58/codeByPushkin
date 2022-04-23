ce481a40f6a64d9046a313a5f7b39769f73fa21e
MegaDen58 <e@w.com>
2022-04-23 10:33:34 +0300

    added

--- Main.java
+++ Main.java
@@ -1,21 +1,34 @@
 package com.company;
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
 import java.util.regex.*;
+import java.util.Scanner;
+
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        FileReader fileReader = new FileReader("input.txt");
-        FileWriter fileWriter = new FileWriter("output.txt");
-        int k = 0;
-        String text = "";
-        while((k=fileReader.read()) != -1){
-            text += (char)k;
-        }
+    public static void main(String[] args) throws Exception{
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите основание: ");
+        int a = in.nextInt();
+        System.out.print("Введите сверхстепень: ");
+        int b = in.nextInt();
+        System.out.print("Введите степень: ");
+        int c = in.nextInt();
+        System.out.println(getHighDegree(a, b, c));
 
-        String a = text.replaceAll("\\//.+", "");
-        String b = a.replaceAll("/\\*(\r\n.+)*\\*/", "");
-        String c = b.replaceAll("\\/\\*([\\S\\s]+)\\*\\/", "");
-        fileWriter.write(c);
-        fileWriter.flush();
+    }
+
+    public static BigInteger getHighDegree(int a, int b, double c){
+        int degree = (int)c;
+        for(int i = 0; i < b; i++){
+            c = Math.pow(c, degree);
+        }
+        BigInteger c1 = BigInteger.valueOf((long)c);
+        BigInteger a1 = BigInteger.valueOf(a);
+        BigInteger result = c1.multiply(a1);
+
+        return result;
     }
 }
